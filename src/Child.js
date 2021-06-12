39c6c6124dab9036ad3d735f5290162af77c9c5c
AllisonAnz <AllisonAnzalone@gmail.com>
2021-06-12 16:58:51 -0500

    Completed Assignment

--- src/Child.js
+++ src/Child.js
@@ -3,10 +3,19 @@ import { getRandomColor } from './randomColorGenerator.js'
 
 class Child extends Component {
   render() {
+    //console.log(this.props) //=> {handleColorChange: ƒ}}
+    
+    // We now want to pass the handleColorChange prop into the Ract event handler
+    // Now if you click on either white rectangle Child comp. will cause the parent componenet to change color
+    
+    //pt2: use the props color from parent comp in child comp, but passing this.props.color
+    // We update the onClick of Child to be a function that invokes this.props.handleColorChange and passes 
+    // in a random color as the argumnet
     return (
       <div
+        onClick={() => this.props.handleColorChange(getRandomColor())}
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.props.color}}
       ></div>
     )
   }
--- src/Parent.js
+++ src/Parent.js
@@ -3,19 +3,41 @@ import { getRandomColor } from './randomColorGenerator.js'
 import Child from './Child'
 
 class Parent extends Component {
-
+  // Parent Comp. has a state called color 
+  // it initially is set to a random color
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      // Store the color of the Child in the state of the Parent componenet so that 
+      // keep track of the color
+      childrenColor: '#FFF'
     }
   }
 
-  render() {
+  //CODED: We set the state 
+  // if we are going to run changeColor() method when either Child componenet is clicked. 
+  // We need to pass this state changing function as a prop to both Child componenets. 
+  // pt2: we have to update the changeColor() method in the Parent to change not just the color state, 
+  // but also the childrenColor. 
+  // to practice sending data BACK to the parent, change our changeColor to take in an argument of newChildColor
+  changeColor = (newChildColor) => {
+    this.setState({
+      color: getRandomColor(),
+      childrenColor: newChildColor
+    })
+  } //Now that the function takes in an argumnet, we'll need to update the onClick of Child to be afunction that 
+    //invokes this.props.handleColorChange and passes in a random color as the argument 
+
+  render() { 
+    //handleColorChange = this.ChangeColor was added from the codealong 
+    //Now Child will have a prop called handleColorChange that is a function. 
+    //pt2: Since the data that represents the color of the two Child comp. lives in Parent, we should pass that data down as props 
+    // color={this.state.childrenColor}
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor} />
+        <Child color={this.state.childrenColor} handleColorChange = {this.changeColor} />
       </div>
     )
   }
